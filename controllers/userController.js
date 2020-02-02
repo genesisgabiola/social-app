c6505e50ea75ad8c5e48c8b86ffda6d7a70e156d
Genesis Gabiola <genesisbritanicogabiola@gmail.com>
2020-02-02 12:27:54 +0800

    Add a session login

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,14 @@
 const User = require('../models/User');
 
-exports.login = function() {};
+exports.login = function(req, res) {
+  let user = new User(req.body);
+  user.login().then(function(result) {
+    req.session.user = { favColor: 'blue', username: user.data.username };
+    res.send(result);
+  }).catch(function(e) {
+    res.send(e);
+  });
+};
 
 exports.logout = function() {};
 
@@ -15,5 +23,9 @@ exports.register = function(req, res) {
 };
 
 exports.home = function(req, res) {
-  res.render('home-guest');
+  if (req.session.user) {
+    res.render('home-dashboard', { username: req.session.user.username });
+  } else {
+    res.render('home-guest');
+  }
 };
