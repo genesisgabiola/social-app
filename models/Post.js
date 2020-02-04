8bc1884fdebca42074a7ba0f4107a66c4dcc8a6b
Genesis Gabiola <genesisbritanicogabiola@gmail.com>
2020-02-04 19:13:47 +0800

    Update to view posts by Author

--- models/Post.js
+++ models/Post.js
@@ -43,14 +43,9 @@ Post.prototype.create = function() {
   });
 };
 
-Post.findSingleById = function(id) {
+Post.reusablePostQuery = function(uniqueOperations) {
   return new Promise(async function(resolve, reject) {
-    if (typeof(id) != 'string' || !ObjectID.isValid(id)) {
-      reject();
-      return;
-    }
-    let posts = await postsCollection.aggregate([
-      { $match: { _id: new ObjectID(id) } },
+    let aggOperations = uniqueOperations.concat([
       { $lookup: { from: 'users', localField: 'author', foreignField: '_id', as: 'authorDocument' } },
       { $project: {
         title: 1,
@@ -58,7 +53,9 @@ Post.findSingleById = function(id) {
         createdDate: 1,
         author: { $arrayElemAt: ['$authorDocument', 0] }
       } }
-    ]).toArray();
+    ]);
+
+    let posts = await postsCollection.aggregate(aggOperations).toArray();
 
     // Clean up author property in each post object
     posts = posts.map(function(post) {
@@ -68,14 +65,35 @@ Post.findSingleById = function(id) {
       };
       return post;
     });
+    resolve(posts);
+  });
+};
+
+Post.findSingleById = function(id) {
+  return new Promise(async function(resolve, reject) {
+    if (typeof(id) != 'string' || !ObjectID.isValid(id)) {
+      reject();
+      return;
+    }
+
+    let posts = await Post.reusablePostQuery([
+      { $match: { _id: new ObjectID(id) } }
+    ]);
 
     if (posts.length) {
-      console.log(posts[0]);
+      // console.log(posts[0]);
       resolve(posts[0]);
     } else {
       reject();
     }
   });
+};
+
+Post.findByAuthorId = function(authorId) {
+  return Post.reusablePostQuery([
+    { $match: { author: authorId } },
+    { $sort: { createdDate: -1 } }
+  ]);
 };
 
 module.exports = Post;
