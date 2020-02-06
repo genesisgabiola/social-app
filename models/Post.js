24fa94433eb7d90fe6ce819cea0e3f41ec49af53
Genesis Gabiola <genesisbritanicogabiola@gmail.com>
2020-02-06 13:09:44 +0800

    Update backend searching db

--- models/Post.js
+++ models/Post.js
@@ -93,6 +93,7 @@ Post.reusablePostQuery = function(uniqueOperations, visitorId) {
     // Clean up author property in each post object
     posts = posts.map(function(post) {
       post.isVisitorOwner = post.authorId.equals(visitorId);
+      post.authorId = undefined;
 
       post.author = {
         username: post.author.username,
@@ -142,6 +143,20 @@ Post.delete = function(postIdToDelete, currentUserId) {
         reject();
       }
     } catch {
+      reject();
+    }
+  });
+};
+
+Post.search = function(searchTerm) {
+  return new Promise(async (resolve, reject) => {
+    if (typeof(searchTerm) == 'string') {
+      let posts = await Post.reusablePostQuery([
+        { $match: { $text: { $search: searchTerm } } },
+        { $sort: { score: { $meta: 'textScore' } } }
+      ]);
+      resolve(posts);
+    } else {
       reject();
     }
   });
